5d9aa2cef763604b470ec29e359bf38e01327335
DavidJurado2017 <davidjuradob10@gmail.com>
2017-02-06 20:27:32 +0100

    matchfunis

---
+++ math_is_fun_2/math_is_fun_2.ino
@@ -0,0 +1,44 @@
+/**********************************************************************************
+**                                                                               **
+**                               Plantilla                                       **
+*                                                                                **
+*                                  Serial                                        **
+*                                                                                **
+*                                                                                **
+*                                                                                **                                             
+*       David Jurado                                           6/02              **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int a = 3;
+int b = 4;
+int h;
+
+//********** Setup ****************************************************************
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);
+  
+  Serial.println("Lets calculate a hypoteneuse");
+
+  h = sqrt(a*a + b*b);
+      
+  Serial.print("a = ");
+  Serial.println(a);
+  Serial.print("b = ");
+  Serial.println(b);
+  Serial.print("h = ");
+  Serial.println(h);
+}
+
+void loop()  // we need this to be here even though its empty
+{
+}
+
+
+//********** Funcions *************************************************************
---
+++ math_is_fun_3/math_is_fun_3/math_is_fun_3.ino
@@ -0,0 +1,44 @@
+/**********************************************************************************
+**                                                                               **
+**                               Plantilla                                       **
+*                                                                                **
+*                                  Serial                                        **
+*                                                                                **
+*                                                                                **
+*                                                                                **                                             
+*       David Jurado                                           6/02              **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int drive_gb = 5;
+int drive_mb;
+
+//********** Setup ****************************************************************
+
+
+void setup()             // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);       // el valor esta a dalt que es 5GB
+  Serial.println(" GB large.");
+
+   drive_mb = 1024 * drive_gb;
+   
+  Serial.print("It can store ");
+  Serial.print(drive_mb);          //et diu que el drive mb que es 1024 el multiplica per el de abans 5
+  Serial.println(" Megabytes!");
+}
+
+
+
+void loop()  // we need this to be here even though its empty
+{
+}
+
+
+//********** Funcions *************************************************************
