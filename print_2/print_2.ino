054c0674644699f30d156816b8e008bf2f838047
DavidJurado2017 <davidjuradob10@gmail.com>
2017-02-13 20:55:39 +0100

    print

--- print_2/print_2.ino
+++ print_2/print_2.ino
@@ -14,38 +14,23 @@
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-int thisByte = 33; 
+int tempAigua = 101;
 //********** Setup ****************************************************************
 
-   
-void setup()    // run once, when the sketch starts
-{ 
-  Serial.begin(9600); 
-  Serial.println("ASCII Table ~ Character Map"); 
-} 
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
 
-void loop()
-{ 
-  Serial.write(thisByte);    
-  
-  Serial.print(", dec: "); 
-  Serial.print(thisByte);      
-  Serial.print(", hex: "); 
-  Serial.print(thisByte, HEX);     
-  Serial.print(", oct: "); 
-  Serial.print(thisByte, OCT);     
-  Serial.print(", bin: "); 
-  Serial.println(thisByte, BIN);   
-
-  if(thisByte == 126)  // if printed last visible character '~'
-  { 
-    while(true)
-    { 
-    } 
+  if ( tempAigua > 100)
+  {
+    Serial.print("Aigua supera els 100C, esta bullint!");
   } 
-  thisByte++;     // go on to the next character
+}
 
-} 
+void loop()   // we need this to be here even though its empty
+{
+}
+
 
 
 
